09ae9619c3918ee2a2d5d2bfe1e24c9e2b3ff629
Chamil <chamiljay88@gmail.com>
2020-08-23 17:17:54 +0530

    second commit

--- src/main/java/com/ic/controller/CustomerController.java
+++ src/main/java/com/ic/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,13 +27,13 @@ public class CustomerController {
 	
 	@GetMapping("/customers")
 	public List<Customer> getAllCustomers(){
-		System.out.println("sdasdasd asd asd asd asd as: >>>>>>>>>>> : ");
+//		System.out.println("sdasdasd asd asd asd asd as: >>>>>>>>>>> : ");
 		return customerRepository.findAll();
 	}
 
 	@PostMapping("/customers")
 	public Customer createCustomer(@RequestBody Customer customer) {
-		System.out.println("sdasdasd asd asd asd asd as: >>>>>>>>>>> : "+customer);
+		
 		return customerRepository.save(customer);
 		
 	}
@@ -43,16 +44,21 @@ public class CustomerController {
 				.orElseThrow(()->new ResourceNotFoundException("employee not exist with id :"+id));
 		return ResponseEntity.ok(customer);
 	}
+	@PutMapping("/customers/{id}")
 	public ResponseEntity<Customer> updateCustomer(@PathVariable Long id,@RequestBody Customer customerDetails){
 		Customer customer =customerRepository.findById(id)
 				.orElseThrow(()->new ResourceNotFoundException("Employee not exit "+id));
 		
+		
 		customer.setCusfullname(customerDetails.getCusfullname());
 		customer.setCusNicNumber(customerDetails.getCusNicNumber());
-		customer.setCusfullname(customerDetails.getCusfullname());
-		customer.setCusfullname(customerDetails.getCusfullname());
-		
+		customer.setCusdob(customerDetails.getCusdob());
+		customer.setCuscontactnumber(customerDetails.getCuscontactnumber());
+		customer.setCusemail(customerDetails.getCusemail());
+		customer.setCusaddress(customerDetails.getCusaddress());
+
 		Customer updateCustomer=customerRepository.save(customer);
+
 		return ResponseEntity.ok(updateCustomer);
 		
 	}
